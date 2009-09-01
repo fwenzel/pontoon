45d616b9c6bb25ce8b8106f24b1021e3ef3b5287
Frederic Wenzel <fwenzel@mozilla.com>
2009-09-01 19:48:13 +0200

    default sqlite db

---
+++ server/sql/pontoon.sql
@@ -0,0 +1,2 @@
+create table users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT);
+create table stats (userid INT, project text, lang text, string text, str_from text, str_to text)
