909f6b6a73b21e2a9fa9cfe8c7dab58e26c64ad2
Frederic Wenzel <fwenzel@mozilla.com>
2009-09-08 12:20:59 -0600

    user management
    --HG--
    extra : convert_revision : d2ffb7a24826a5b38c2b74c96aa4b9f639e0b9a1

--- server/sql/pontoon.sql
+++ server/sql/pontoon.sql
@@ -1,2 +1,3 @@
 create table users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT);
+create unique index username on users (username)
 create table stats (userid INT, project text, lang text, string text, str_from text, str_to text)
